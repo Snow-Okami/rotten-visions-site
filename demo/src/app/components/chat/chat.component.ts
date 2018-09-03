abb8bc0b29e716d7663b57a198e9f8ad28145f68
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-09-03 13:02:21 +0530

    framework is almost ready with it...

--- demo/src/app/components/chat/chat.component.ts
+++ demo/src/app/components/chat/chat.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  public msgview = false;
+  public user = {
+    fname: '',
+    lname: ''
+  };
 
   constructor() { }
 
