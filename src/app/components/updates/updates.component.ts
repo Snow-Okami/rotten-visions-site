89f5158145906de60e69615b239deeaa0d6a1480
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-10-08 13:13:34 +0530

    Working on page infinite scroll actions.

--- src/app/components/updates/updates.component.ts
+++ src/app/components/updates/updates.component.ts
@@ -1,5 +1,7 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
+
+let that;
 
 @Component({
   selector: 'app-updates',
@@ -9,17 +11,33 @@ import { MediaMatcher } from '@angular/cdk/layout';
 export class UpdatesComponent {
   public title = 'Rotten Visions | Updates';
   public mobileQuery: MediaQueryList;
-  public array = Array.from({length: 10}, (_, i) => `Coming soon...`);
+  public array = Array.from({length: 20}, (_, i) => `Coming soon...`);
+  @ViewChild('loadScroll') loadScroll: any;
+  @ViewChild('updateContainer') updateContainer: any;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+
+    that = this;
   }
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    window.addEventListener('scroll', this.onScrollDown);
+  }
+
   private _mobileQueryListener: () => void;
+
+  onScrollUp() {
+    console.log('scroll down detected!');
+  }
+
+  onScrollDown() {
+    console.log(window.innerHeight, that.loadScroll.nativeElement.getBoundingClientRect().top);
+  }
 
 }
 
