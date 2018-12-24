ccc3c504a235e7d6ff6330eb9b2120fdcd1bbc70
Abhisek Dutta <abhisek@capitalnumbers.com>
2018-12-24 17:18:49 +0530

    Updated to v 2.8.3 stable.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,7 +10,7 @@ export class FooterComponent implements OnInit {
    * 
    * @description version is the app version.
    */
-  public version: string = '2.8.2';
+  public version: string = '2.8.3';
 
   constructor() { }
 
