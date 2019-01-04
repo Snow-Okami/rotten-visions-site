ac32397a88f973f06a8211cfd4f1d1d04cf40d53
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-01-04 14:52:42 +0530

    Updated to version 2.9.3 for production.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
   /**
    * @description version is the app version.
    */
-  public version: string = '2.9.2';
+  public version: string = '2.9.3';
 
   constructor() { }
 
