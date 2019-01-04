c3f8282ab5ace16cbd555b78e8bff336e1c787dd
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-01-04 16:00:00 +0530

    Updated with writable updates UI fixed.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
   /**
    * @description version is the app version.
    */
-  public version: string = '2.9.3';
+  public version: string = '2.9.4';
 
   constructor() { }
 
