94d48bb3733bd14aa1b7a41a7c7ec7b38dfae3ba
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-01-08 16:32:32 +0530

    Updated CSS files with stable update 2.9.7 for production.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
   /**
    * @description version is the app version.
    */
-  public version: string = '2.9.6';
+  public version: string = '2.9.7';
 
   constructor() { }
 
