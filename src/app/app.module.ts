2e9a4debe7d6f49a08709a8e1529af6db8674ac2
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-04-26 13:01:00 +0530

    added desysia site.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-// import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-import { MaterialModule } from './modules/material/material.module';
-
-import { RoutingModule } from './modules/routing/routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// const config: SocketIoConfig = { url: 'http://localhost:3333', options: {} };
 
 @NgModule({
   declarations: [
@@ -17,13 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    RoutingModule,
-    HttpClientModule,
-    // SocketIoModule.forRoot(config)
+    AppRoutingModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
--- src/app/pipes/updatefilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UpdatefilterPipe } from './updatefilter.pipe';
-
-describe('UpdatefilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UpdatefilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
