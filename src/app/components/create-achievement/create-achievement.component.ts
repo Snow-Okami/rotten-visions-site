d754b25d2e94e73a39e0d106ecdff8be7c9659fa
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-06-11 12:36:28 +0530

    added create achievement template.

--- src/app/components/create-achievement/create-achievement.component.ts
+++ src/app/components/create-achievement/create-achievement.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { User } from '../../interfaces/user';
 
 import { HttpService } from '../../services/http.service';
 import { StoreService } from '../../services/store.service';
+import { ValidatorsService } from '../../services/validators.service';
 
 let that: any;
 
@@ -21,18 +24,34 @@ export class CreateAchievementComponent implements OnInit {
 
   private user: User;
 
+  public disableClick: boolean = false;
+
   public isAdmin: boolean = false;
+
+  public achievementForm: FormGroup;
+
+  public hideImage: boolean = true;
+  @ViewChild('dropImage') image: ElementRef;
+  @ViewChild('dropFile') file: ElementRef;
 
   constructor(
     public changeDetectorRef: ChangeDetectorRef,
     public media: MediaMatcher,
     private http: HttpService,
     private store: StoreService,
-    public router: Router
+    public router: Router,
+    public form: FormBuilder,
+    private regx: ValidatorsService,
+    public page: Location
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 840px)');
     // this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.achievementForm = form.group({
+      title: new FormControl({ value: '', disabled: false }, [ Validators.required, this.regx.title ]),
+      description: new FormControl({ value: '', disabled: false }, [ Validators.required, this.regx.description ])
+    });
 
     that = this;
   }
@@ -66,6 +85,37 @@ export class CreateAchievementComponent implements OnInit {
      */
     this.progressBar.classList.add('hidden');
 
+  }
+
+  async editOrCreate(e: Event) {
+    console.log('form submit event', e);
+
+    /**
+     * @description Disable buttons, inputs & enable loader.
+     */
+    this.disableClick = true;
+    this.progressBar.classList.remove('hidden');
+  }
+
+  async showImage(e: Event) {
+    let image = this.image.nativeElement;
+    let file = this.file.nativeElement;
+    let reader  = new FileReader();
+
+    reader.addEventListener("load", function () {
+      image['src'] = reader.result;
+      that.hideImage = false;
+    }, false);
+
+    if (file['files'][0]) {
+      reader.readAsDataURL(file['files'][0]);
+    }
+  }
+
+  removeImage(): void {
+    this.hideImage = true;
+    this.image.nativeElement['src'] = '';
+    this.file.nativeElement.value = null;
   }
 
 }
