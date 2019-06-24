6c02643186425d3605a3790833526e902633a59f
Abhisek Dutta <abhisek@capitalnumbers.com>
2019-06-24 13:39:56 +0530

    added view news modular component

---
+++ src/app/components/view-news/view-news.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewNewsRoutingModule } from './view-news-routing.module';
+import { ViewNewsComponent } from './view-news.component';
+
+/**
+ * @description MaterialModule is for Material theme.
+ * @description Load images lazely.
+ * @description SharedModule contains the shared Components & Pipes throughout Psynapsus.
+ */
+import { MaterialModule } from '../../modules/material/material.module';
+import { SharedModule } from '../../modules/shared/shared.module';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+
+@NgModule({
+  declarations: [ViewNewsComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MaterialModule,
+    LazyLoadImageModule,
+    ViewNewsRoutingModule
+  ]
+})
+export class ViewNewsModule { }
